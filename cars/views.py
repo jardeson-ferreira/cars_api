8d857a4a644280430cc9870130b45a9d51e21c53
jardeson-ferreira <jardesonnferreira@gmail.com>
2024-10-08 23:01:56 -0300

    feat: create permissions for users

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,9 @@
 from dj_rql.drf import RQLFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from cars.filters import BrandFilterClass, CarFilterClass
 from cars.models import Brand, Car
 from cars.serializers import BrandModelSerializer, CarModelSerializer
+from cars.permissions import CarOwnerPermission
 
 
 class BrandModelViewSet(viewsets.ModelViewSet):
@@ -17,3 +18,4 @@ class CarModelViewSet(viewsets.ModelViewSet):
     serializer_class = CarModelSerializer
     filter_backends = [RQLFilterBackend]
     rql_filter_class = CarFilterClass
+    permission_classes = [permissions.DjangoModelPermissions, CarOwnerPermission,]
